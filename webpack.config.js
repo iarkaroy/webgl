87f9e270c02362c4147c80981ed222a7685dc871
Arka Roy <arkaroy@Arkas-MacBook-Pro.local>
2018-03-23 13:30:25 +0530

    Added packages and setup configs

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/js/main.js'
+    },
+    output: {
+        path: __dirname,
+        filename: './dist/[name].js'
+    },
+    devtool: 'source-map',
+    plugins: [
+        new ExtractTextPlugin('./dist/main.css')
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env']
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'postcss-loader',
+                        'sass-loader'
+                    ]
+                })
+            }
+        ]
+    }
+}
